9e6f1f594b62d6551465f612ea2262aac258c5bc
Melvin Delorme <melvin.delorme12@gmail.com>
2024-06-20 12:02:32 +0200

    Légères modifications

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -4,11 +4,14 @@ const userController = require('../controllers/userController');
 const authenticateJWT = require('../middlewares/jwt');
 const checkStatus = require('../middlewares/checkStatus');
 
+// Inscription - Connection
 router.post('/register', userController.userRegister);
 router.post('/login', userController.userLogin);
 
-
+// Utilisateur
 router.get('/profile', authenticateJWT, checkStatus, userController.getUserProfile);
+
+// Administrateur
 router.get('/adminProfile', authenticateJWT, userController.getAdminProfile);
 
 module.exports = router;
