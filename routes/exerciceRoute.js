6dbe8c48f47e910662fd4a84e03f3f4294d8d67c
Melvin Delorme <melvin.delorme12@gmail.com>
2024-07-05 22:36:52 +0200

    Get one exercice user

--- routes/exerciceRoute.js
+++ routes/exerciceRoute.js
@@ -5,5 +5,6 @@ const authenticateJWT = require('../middlewares/jwt');
 const { checkRoleUser } = require("../middlewares/checkRole");
 
 router.get('/getall', authenticateJWT, checkRoleUser, exerciceController.getAllExercises);
+router.get('/getone/:id', authenticateJWT, checkRoleUser, exerciceController.getOneExercise);
 
 module.exports = router;
