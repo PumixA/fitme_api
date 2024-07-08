3d93a11b0b45372d3f9be8cd5076e147a515e8fe
Melvin Delorme <melvin.delorme12@gmail.com>
2024-07-08 16:41:39 +0200

    Gestion add et edit exercice sur les photos

--- controllers/exerciceController.js
+++ controllers/exerciceController.js
@@ -1,9 +1,7 @@
 const Exercice = require('../models/exerciceModel');
 const path = require('path');
 const fs = require('fs').promises;
-
 const sharp = require('sharp');
-const { v4: uuidv4 } = require('uuid');
 
 exports.getAllExercises = async (req, res) => {
     const { id_groupe_musculaire } = req.query;
@@ -50,7 +48,6 @@ exports.addExercise = async (req, res) => {
         await newExercice.save();
 
         if (file) {
-            console.log('Original filename:', file.originalname);
             const fileExtension = path.extname(file.originalname).toLowerCase();
             const validExtensions = ['.jpg', '.png'];
 
@@ -63,10 +60,8 @@ exports.addExercise = async (req, res) => {
             const uploadPath = path.join(__dirname, '..', 'uploads', 'exercices', photoFile);
             const tempCropPath = path.join(__dirname, '..', 'uploads', 'exercices', `temp_${photoFile}`);
 
-            console.log('Saving file with _id extension...');
             await fs.rename(file.path, uploadPath);
 
-            console.log('Cropping and resizing the image...');
             await sharp(uploadPath)
                 .resize(500, 500, {
                     fit: sharp.fit.cover,
@@ -74,10 +69,7 @@ exports.addExercise = async (req, res) => {
                 })
                 .toFile(tempCropPath);
 
-            console.log('Overwriting the original file with the cropped image...');
             await fs.rename(tempCropPath, uploadPath);
-
-            console.log('Processed file path:', uploadPath);
 
             newExercice.photo = photoFile;
             newExercice.photo_original_name = file.originalname;
@@ -88,7 +80,6 @@ exports.addExercise = async (req, res) => {
 
         res.status(201).json({ message: 'Exercice créé avec succès', newExercice });
     } catch (err) {
-        console.error('Error:', err.message);
         if (file) {
             await fs.unlink(file.path).catch(console.error);
         }
@@ -125,29 +116,29 @@ exports.editExercise = async (req, res) => {
                 return res.status(400).json({ message: 'Uniquement JPG ou PNG' });
             }
 
-            const newFileName = `${exercice._id}${fileExtension}`;
-            const uploadPath = path.join(__dirname, '..', 'uploads', 'exercices', newFileName);
+            const photoFile = `${exercice._id}${fileExtension}`;
+            const uploadPath = path.join(__dirname, '..', 'uploads', 'exercices', photoFile);
+            const tempCropPath = path.join(__dirname, '..', 'uploads', 'exercices', `temp_${photoFile}`);
 
-            if (exercice.photo) {
-                const oldPhotoPath = path.join(__dirname, '..', 'uploads', 'exercices', exercice.photo);
-                await fs.unlink(oldPhotoPath).catch(console.error);
-            }
+            await fs.rename(file.path, uploadPath);
 
-            await sharp(file.path)
+            await sharp(uploadPath)
                 .resize(500, 500, {
                     fit: sharp.fit.cover,
                     position: sharp.strategy.entropy
                 })
-                .toFile(uploadPath);
+                .toFile(tempCropPath);
 
-            await fs.unlink(file.path);
+            await fs.rename(tempCropPath, uploadPath);
 
-            exercice.photo = newFileName;
+            exercice.photo = photoFile;
+            exercice.photo_original_name = file.originalname;
+            exercice.photo_extension = fileExtension;
         }
 
         await exercice.save();
 
-        res.status(200).json({ message: 'Exercice mis a jour avec succés', exercice });
+        res.status(200).json({ message: 'Exercice mis à jour avec succès', exercice });
     } catch (err) {
         if (file) {
             await fs.unlink(file.path).catch(console.error);
@@ -155,4 +146,3 @@ exports.editExercise = async (req, res) => {
         res.status(500).json({ message: err.message });
     }
 };
-
